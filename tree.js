d2bc3c671fc059a93d34f5f6de8b53ac9a50fc55
samah-abujwaied <samah.abujweed@yahoo.com>
2021-10-02 14:43:11 +0300

    treeee

--- tree.js
+++ tree.js
@@ -86,28 +86,6 @@ class BinarySearchTree {
 
 }
 /////// ✅ ✅  ✅ ✅4
-function contains(root, value) {
-    if (!root) {
-        return false;
-    }
-    let searchValue = (current, value) => {
-        if (current.value === value) {
-            return true;
-        } else if (current.value > value) {
-            if (!current.left) {
-                return false;
-            }
-            return searchValue(current.left, value);
-        } else {
-            if (!current.right) {
-                return false;
-            }
-            return searchValue(current.right, value);
-        }
-    };
-
-    return searchValue(root, value);
-}
 function ifContains(root,value){
     if(!root)
     return;
@@ -167,23 +145,37 @@ function findMinNumber(root) {
     if (rres < min) min = rres;
     return min;
 
-  
 }
 ///////////////////////
-let isSymmetric = function(root) {
+ function isSymmetric(root) {
     if (root == null) return true;
+
+     let isMirror =(leftSub, rightSub)=> {
+        if (leftSub == null && rightSub == null) return true;
+        if (leftSub == null || rightSub == null) return false;
+    
+        return (leftSub.value === rightSub.value 
+                && isMirror(leftSub.left, rightSub.right) 
+                && isMirror(leftSub.right, rightSub.left))
+    }
+
     return isMirror(root.left, root.right);
 };
 
-const isMirror = function(leftSub, rightSub) {
-    if (leftSub == null && rightSub == null) return true;
-    if (leftSub == null || rightSub == null) return false;
+function identicalTrees(a, b)
+{
 
-    return (leftSub.val === rightSub.val 
-            && isMirror(leftSub.left, rightSub.right) 
-            && isMirror(leftSub.right, rightSub.left))
+    if (a == null && b == null)
+        return true;
+   
+    if (a != null && b != null)
+        return (a.value == b.value
+                && identicalTrees(a.left, b.left)
+                && identicalTrees(a.right, b.right));
+
+    return false;
 }
-
+/////////////////////////////////////////////////////    
 function BreadthFirst  (root) {
     const result = [];
     const q = [root];
@@ -202,38 +194,8 @@ function BreadthFirst  (root) {
     }
     else return 'Sorry there is no root';
 }
-function fizzBuzzTree(root) {
-    const result =[]; 
-        let FBT = (node) => {
-            if(node){  
-              if (node.left) 
-              {  
-                  FBT(node.left); 
-              }
-                        if (node.value % 15 === 0) 
-                        {   
-                            result.push('FizzBuzz')
-                        }
-                        else if (node.value % 5 === 0) {
-                            result.push('Buzz')
-                            }
-                        else if (node.value % 3 === 0) {
-                            
-                            result.push('Fizz')
-                            }
-                            else{
-                                result.push(node.value) 
-                            }
-
-                if (node.right) { 
-                    FBT(node.right); }     
-                  };
-            }
-            FBT(root);
-            return result ;
-};
-    
-function SumofNumbersBT(root) {
+//////////////////////////////////////////////////   
+function SumofNumbersBT(root) { // for all nodes tree
     let result=0 ;
     const q = [root];
     if(root){
@@ -251,7 +213,7 @@ function SumofNumbersBT(root) {
     }
     else return 'Sorry there is no root';
 }
-ks
+///////////////////////////////////////////////////
 function printZigZagTraversal(rootNode)
 {
  let result =[];
@@ -283,25 +245,221 @@ function printZigZagTraversal(rootNode)
     }
     return result;
 }
+/////////////////////////////////////////////////////
+function sum(node) 
+{
+    if(!node)
+    {
+        return false;
+    }
+    return (sum(node.left) + node.value+sum(node.right));
+}
+ 
+function isSumTree(node)//// for each root should be the sum for all child
+{
+    let ls,rs;
 
-let one = new Node(1);
-let two = new Node(2);
-let three = new Node(3);
-let four = new Node(4);
-let five = new Node(5);
-let six = new Node(6);
-let seven = new Node(7);
-let eight = new Node(8);
-let nine = new Node(9);
+    if(!node || (!node.left && !node.right))  return true;
+  
+    ls = sum(node.left);
+    rs = sum(node.right);
+    if((node.value == ls + rs) && isSumTree(node.left) != 0 && isSumTree(node.right) != 0)
+    {
+        return true;
+    }
+    return false;
+}
+//////////////////////////////////////////////
+function isLeaf(node)
+{
+    if (node == null)
+        return false;
+    if (node.left == null && node.right == null)
+        return true;
+    return false;
+}
+
+function leftLeavesSum(node)
+{
+ 
+    let res = 0;
+    if (node)
+    {
+        if (isLeaf(node.left))
+            res += node.left.value;
+        else 
+            res += leftLeavesSum(node.left);
+        res += leftLeavesSum(node.right);
+    }
+
+    return res;
+}
+/////////////////////////////////////////
+
+function isLeafRight(node)
+{
+    if (node == null)
+        return false;
+    if (node.left == null && node.right == null)
+        return true;
+    return false;
+}
+
+function rightLeavesSum(node)
+{
+ 
+    let res = 0;
+    if (node)
+    {
+     
+        if (isLeafRight(node.right))
+            res += node.right.value;
+        else 
+            res += rightLeavesSum(node.right);
+        res += rightLeavesSum(node.left);
+    }
+
+    return res;
+}
+//////////////////////////////////////////////
+function areIdentical(root1, root2)
+{
+    if (!root1 && !root2 ) return true;
+    if (!root1|| !root2) return false;
+
+    return (root1.value == root2.value
+            && areIdentical(root1.left, root2.left)
+            && areIdentical(root1.right, root2.right));
+}
+
+function isSubtree(T,  S)
+{
+    if (S == null) return true;
+
+    if (T == null)  return false;
+
+    if (areIdentical(T, S))  return true;
+
+    return isSubtree(T.left, S) || isSubtree(T.right, S);
+}
+////////////////////////////////////////////////////
+function  convertmirror(node)
+{
+    if (node == null)
+        return node;
+
+    let left = convertmirror(node.left);
+    let right = convertmirror(node.right);
+
+    node.left = right;
+    node.right = left;
+
+    return node;
+}
+/////////////////////////////////////////
+
+function rangeSumBST(root,low,high)
+{  
+    let sum1 = 0;
+    if (!root) return 0;
+    let q = [root];
+    while (q.length!=0)
+    {
+        let curr = q.shift();
+
+        if (curr.value >= low && curr.value <= high)
+            sum1 += curr.value;
+        
+        if (curr.left && curr.value > low)
+            q.push(curr.left);
+  
+        if (curr.right != null &&
+            curr.value < high)
+
+            q.push(curr.right);
+    }
+  
+    return sum1;
+}
+
+////////////////////////////////////////////////
+function MergeTrees(t1, t2)
+{
+    if (t1 == null)
+        return t2;
+    if (t2 == null)
+        return t1;
+         
+    t1.value += t2.value;
+    t1.left = MergeTrees(t1.left, t2.left);
+    t1.right = MergeTrees(t1.right, t2.right);
+    return t1;
+}
+/////////////////////////////////////////////////
+
+
+/////////////////////////////////////// consolsssssssssssssssssssssssssssssssssssssssseeeeeeeeeeeeee
+let one = new Node(26);
+let two = new Node(10);
+let three = new Node(1);
+let four = new Node(6);
+let five = new Node(4);
+let six = new Node(11);
+let seven = new Node(4);
+// let eight = new Node(8);
+// let nine = new Node(9);
 
 one.left = two;
 one.right = three;
-two.left = six;
-two.right = seven;
-seven.left = eight;
-seven.right = nine;
-three.left = four;
-three.right = five;
+two.left = four;
+two.right = five;
+three.left = seven;
+three.right = six;
+
+let one1 = new Node(10);
+let two2 = new Node(6);
+let three3 = new Node(4);
+let four4 = new Node(3);
+let five5 = new Node(3);
+let six6 = new Node(4);
+let seven7 = new Node(4);
+// let eight = new Node(8);
+// let nine = new Node(9);
+
+one1.left = two2;
+one1.right = three3;
+two2.left = four4;
+two2.right = six6;
+three3.left = seven7;
+three3.right = five5;
+
+let root1 =new Node(1);
+root1.left = new Node(2);
+root1.right = new Node(3);
+root1.left.left = new Node(4);
+root1.left.right =new  Node(5);
+root1.right.right =new  Node(6);
+
+let root2 = new Node(4);
+root2.left = new Node(1);
+root2.right =new  Node(7);
+root2.left.left =new  Node(3);
+root2.right.left =new Node(2);
+root2.right.right =new Node(6);
+
+console.log('is symmitric ------->',isSymmetric(one));
+console.log('identicalTrees',identicalTrees(one, one1));
+console.log('isSumTree---->',isSumTree(one));
+console.log('leftLeavesSum------->',leftLeavesSum(one));
+console.log('rightLeavesSum------->',rightLeavesSum(one));
+console.log('isSubtree-------->',isSubtree(one,one1));
+console.log(' mirror(root)----------->', convertmirror(one1));
+console.log('MergeTrees----------------->',MergeTrees(root1, root2));
+
+
+
+// three.left = four;
+// three.right = five;
 console.log(preOrder(one));
 console.log(inOrder(one));
 console.log(postOrder(one));
@@ -314,7 +472,7 @@ BST.add(10);
 BST.add(7);
 BST.add(8);
 BST.add(11);
-
+console.log('rangeSumBST---------------->',rangeSumBST(BST.root,5,8));
 // add(BST, 4);
 // BST.add(2);
 // BST.add(5);
@@ -327,80 +485,3 @@ console.log('ifContains',ifContains(BST.root ,9));
 console.log('ifContains',ifContains(one,9));
 console.log('ifContainsBT',ifContainsBT(one,11));
 console.log('ifContainsBST',ifContainsBT(BST.root,11));
-// console.log('SumofNumbersBT',SumofNumbersBT(one));
-// console.log('printZigZagTraversal',printZigZagTraversal(one));
-// console.log('addBT------' , addBT(one));
-// console.log(BST);
-// console.log(contains(BST.root, 6));
-
-
-// function findMaxNumber(root) {
-//     if (!root)
-//     return false;
-//     let max = root.value;
-//     console.log('max --------',max);
-    
-//     let lres = findMaxNumber(root.left);
-//     console.log('lres--------',lres);
-//     let rres = findMaxNumber(root.right);
-//     console.log('rres--------',rres);
-//     if (lres > max){
-//         console.log('left reespons-->',lres );
-//         console.log('max left befor reespons-->',max );
-//         max = lres;
-//         console.log('max left after reespons-->',max );
-//     }
-        
-//     if (rres > max)
-//     {   console.log('right reespons-->',rres );
-//     console.log('max left befor reespons-->',max );
-//     max = rres;
-//     console.log('right max reespons-->',max );
-//     console.log('max left after reespons-->',max );
-
-
-//     }
-//     return max;
-// }
-
-// function BreadthFirst  (root) {
-//     const result = [];
-//     const q = [root];
-//     let switching = false;
-//     let cur ;
-//     if(root){
-//         while (q.length > 0) {
-//        console.log('----',q.length);
-//           if(switching)  {
-//             cur = q.shift()
-//             result.push(cur.value)
-//             switching = false;
-//             if (cur.left){
-//                 q.push(cur.left)
-                
-//             }
-//             if (cur.right){
-//                 q.push(cur.right)
-                
-//             }
-//           }
-//         else { 
-//             cur = q.shift()
-//             result.push(cur.value)
-//             switching = true;
-//             if (cur.right){
-//                 q.push(cur.left)
-                
-//             }
-//             if (cur.left){
-//                 q.push(cur.right)
-                
-//             }
-//         }
-    
-//         };
-//         return result
-
-//     }
-//     else return 'Sorry there is no root';
-// }
